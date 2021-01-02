ad93cd41a3002dab24ad0dcd85e68bc348d86158
Can <esercankutay@users.noreply.github.com>
2021-01-02 19:40:03 +0300

    pretteir formatting

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
-import { hot } from 'react-hot-loader';
-import * as React from 'react';
-import 'antd/dist/antd.min.css'
-import {Card} from 'antd'
+import { hot } from "react-hot-loader";
+import * as React from "react";
+import "antd/dist/antd.min.css";
+import { Card } from "antd";
 
-
-class App extends React.Component{
-    render(){
-        return <Card title="Cart deneme"><p>Deneme içerik kart içeriği</p></Card>
-    }
+class App extends React.Component {
+  render() {
+    return (
+      <Card title="Cart deneme">
+        <p>Deneme içerik kart içeriği</p>
+      </Card>
+    );
+  }
 }
 
 export default hot(module)(App);
